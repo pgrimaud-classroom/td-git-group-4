79677c8a3551b3a4c13b5a121ee5f6d34c21431e
Yannis <yannis.baltus@outlook.com>
2020-10-15 14:51:31 +0200

    Student1: Exo1,2,3

---
+++ Student1/exo123.php
@@ -0,0 +1,18 @@
+<?php
+$x = 10;
+$y = 20;
+echo "x=".$x.",\n";
+echo "y=".$y.",\n";
+echo "(x+y)=".($x+$y).",\n";
+echo "(x*2)=".($x*2).",\n";
+
+$x = rand(1,5);
+if($x==1)
+{
+    echo "Hello \n";
+}elseif($y==2)
+{
+    echo "Salut \n";
+}else{
+    echo "Yo \n";
+}
